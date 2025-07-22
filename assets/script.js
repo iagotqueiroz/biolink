86ff82acebc5505ad9a5a918c32fc1986349b4a1
Iago Tolentino Queiroz <147836102+iagotqueiroz@users.noreply.github.com>
2025-07-22 18:42:10 -0300

    :tada: initial commit

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if(html.classList.contains('light')) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', 'Avatar2.png')
+
+    img.setAttribute(
+      "alt",
+      "Foto de Iago Queiroz de cabelo cacheado penteado, usando camisa branca, sentado em uma poltrona com o notebook na mesa em uma loja de porcelanatos"
+    )
+    } else {
+      //se tiver  sem light mode, manter a imagem normal
+      img.setAttribute('src', 'Avatar.png')
+
+      img.setAttribute(
+        "alt",
+        "Foto de Iago Queiroz de cabelo cacheado penteado, usando camisa branca e fundo cinza"
+      )
+    }
+
+
+}
